66b03e37ca3af2686584b87d4cb95a124fb9e432
asiadigitalhub <51394854+asiadigitalhub@users.noreply.github.com>
2024-09-17 10:37:08 +0700

    Update ReactNativeAD.js

--- src/ReactNativeAD.js
+++ src/ReactNativeAD.js
@@ -1,5 +1,5 @@
 // @flow
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import CONST from './const.js'
 import Timer from 'react-timer-mixin'
 import log from './logger'
